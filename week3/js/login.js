44cd649603fd3addde08efa2988b757a51d1ab71
forest079 <bluegx08@gmail.com>
2023-02-03 16:17:40 +0800

    第三週更新

---
+++ week3/js/login.js
@@ -0,0 +1,36 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+// Api 位置
+const apiUrl = `https://vue3-course-api.hexschool.io/V2/`;
+const apiPath = `ryanpro`;
+
+// 建立 Vue 元件
+createApp({
+  data() {
+    return {
+      loginData: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    login() {
+      // console.log(this.loginData);
+      const url = `${apiUrl}admin/signin`;
+      axios
+        .post(url, this.loginData)
+        .then((res) => {
+          const { expired, token } = res.data;
+          // console.log(expired, token);
+          document.cookie = `ryanpro=${token}; expires=${new Date(expired)};`;
+
+          window.location = `./products.html`;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  mounted() {
+    console.log(`${apiUrl}admin/signin`);
+  },
+}).mount('#app');
---
+++ week3/js/products.js
@@ -0,0 +1,106 @@
+// 引入 vue
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+const apiUrl = 'https://vue3-course-api.hexschool.io/v2/';
+const apiPath = 'ryanpro';
+// /v2/api/{api_path/admin/products
+let productModal = {};
+let delProductModal = {};
+
+createApp({
+  data() {
+    return {
+      products: [],
+      targetProduct: {
+        imagesUrl: [],
+      },
+      isNew: false,
+    };
+  },
+  methods: {
+    checkAdmin() {
+      const url = `${apiUrl}api/user/check`;
+      axios
+        .post(url)
+        .then(() => {
+          this.getProducts();
+        })
+        .catch((err) => {
+          alert(err.data.message);
+          window.location = 'login.html';
+        });
+    },
+    getProducts() {
+      const url = `${apiUrl}api/${apiPath}/admin/products/all`;
+      axios
+        .get(url)
+        .then((response) => {
+          // console.log(response);
+          this.products = response.data.products;
+          // console.log(this.products);
+        })
+        .catch((err) => {
+          console.log(err.data.message);
+        });
+    },
+    openModal(state, product) {
+      console.log(state);
+      if (state === 'create') {
+        productModal.show();
+        this.isNew = true;
+
+        // 帶入初始化資料
+        this.targetProduct = {
+          imagesUrl: [],
+        };
+      } else if (state === 'edit') {
+        productModal.show();
+        this.isNew = false;
+        // 如果是編輯狀態會帶入編輯資料
+        this.targetProduct = { ...product };
+      } else if (state === 'delete') {
+        delProductModal.show();
+        this.isNew = false;
+        // 如果是刪除狀態會帶入刪除資料
+        this.targetProduct = { ...product }; // 取得ID
+      }
+    },
+    updateProduct() {
+      // console.log(`${apiUrl}api/${apiPath}/admin/product`);
+      let url = `${apiUrl}api/${apiPath}/admin/product`;
+      // 利用 isNew 判斷 API 如何運行
+      let method = 'post';
+      if (!this.isNew) {
+        url = `${apiUrl}api/${apiPath}/admin/product/${this.targetProduct.id}`;
+        method = 'put';
+      }
+      axios[method](url, { data: this.targetProduct }).then((res) => {
+        this.getProducts();
+        productModal.hide(); // 關掉 modal
+      });
+    },
+    deleteProduct() {
+      const url = `${apiUrl}api/${apiPath}/admin/product/${this.targetProduct.id}`;
+      axios.delete(url).then(() => {
+        this.getProducts();
+        delProductModal.hide(); // 關掉 modal
+      });
+    },
+  },
+  mounted() {
+    // 取出 token
+    const tokenValue = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('ryanpro='))
+      ?.split('=')[1];
+    axios.defaults.headers.common['Authorization'] = tokenValue;
+    this.checkAdmin();
+
+    // console.log(bootstrap);
+    // 初始化 Bootstrap Modal
+    productModal = new bootstrap.Modal('#productModal');
+    delProductModal = new bootstrap.Modal('#delProductModal');
+    // 呼叫 Modal
+    // productModal.show();
+  },
+}).mount('#app');
