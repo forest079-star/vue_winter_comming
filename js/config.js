d221f993204ed4e5fb469f72ec0c1b387808d8b1
forest079 <bluegx08@gmail.com>
2023-02-21 17:53:41 +0800

    week 5 功能大略完成更新

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2/';
-const apiPath = 'ryanpro';
+// const apiPath = 'ryanpro';
+const apiPath = 'casper-hexschool';
 export { 
   apiUrl, apiPath 
 };
